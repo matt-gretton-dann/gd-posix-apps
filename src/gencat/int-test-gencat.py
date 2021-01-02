f8b51738f68efac7234018e3fcf53280d14158a9
Matthew Gretton-Dann <matt@gretton-dann.org.uk>
2021-01-02 21:08:52 +0000

    Add some more testing of gencat

--- src/gencat/int-test-gencat.py
+++ src/gencat/int-test-gencat.py
@@ -11,6 +11,7 @@ tester = int_tests.TestRunner()
 
 tester.run_test(
     [tester.exe()], test_name="Empty command line", expected_rc=1, expected_stdout="")
+
 tester.run_test(
     [tester.exe(), '-', '-'], test_name="Empty input",
     expected_rc=0,
@@ -18,6 +19,7 @@ tester.run_test(
     expected_stdout=b"MSG\0\1\0\0\0\0\0\0\0\0\0\0\0\x18\0\0\0\0\0\0\0",
     expected_stderr=b'')
 
+# Generate a basic catalogue
 test_output = tester.output_file('test.msg')
 test_input = tester.input_file('basic-input.cat')
 skip_next = tester.run_test(
@@ -27,5 +29,16 @@ skip_next = tester.run_test(
     expected_stdout='', expected_stderr='', files=[test_output])
 tester.compare_files(
     test_output, tester.input_file('basic-input.msg'), skip=skip_next)
+
+# Check that trying to amend it with an empty file does nothing.
+skip_next2 = tester.run_test(
+    [tester.exe(), test_output, '-'],
+    test_name="Empty input doesn't modify",
+    expected_rc=0,
+    stdin='$ empty msgcat\n',
+    expected_stdout='', expected_stderr='', files=[test_output], skip=skip_next)
+tester.compare_files(
+    test_output, tester.input_file('basic-input.msg'), skip=skip_next2)
+
 
 tester.summarize()
