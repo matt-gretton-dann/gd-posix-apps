0a5dfef376789f3a047a92ab82eecacf69866da2
Matthew Gretton-Dann <matt@gretton-dann.org.uk>
2021-01-09 19:13:13 +0000

    Ensure we clang format utility sources
    This adds clang formatting targets and corrects the formatting of
    true-false.c

--- src/true-false/true-false.c
+++ src/true-false/true-false.c
@@ -3,17 +3,18 @@
  *  \author Copyright 2020, Matthew Gretton-Dann
  *  SPDX-License-Identifier: Apache-2.0
  *
- * Requires TRUE_FALSE_EXIT_CODE to be defined on the command line.  Will be the exit code used by 
+ * Requires TRUE_FALSE_EXIT_CODE to be defined on the command line.  Will be the exit code used by
  * the program.
  */
 
 #ifndef TRUE_FALSE_EXIT_CODE
-#define TRUE_FALSE_EXIT_CODE 1
-#error "Must define TRUE_FALSE_EXIT_CODE"
+#  define TRUE_FALSE_EXIT_CODE 1
+#  error "Must define TRUE_FALSE_EXIT_CODE"
 #endif
 
-int main(void) {
-    /* Whilst not specified by POSIX most true/false implementations ignore command line arguments.
-       We do the same.  */
-    return TRUE_FALSE_EXIT_CODE;
+int main(void)
+{
+  /* Whilst not specified by POSIX most true/false implementations ignore command line
+   * arguments. We do the same.  */
+  return TRUE_FALSE_EXIT_CODE;
 }
