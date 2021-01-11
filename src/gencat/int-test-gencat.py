9dcf339b08728369ff7e15b17b8d04f33ea0b531
Matthew Gretton-Dann <matt@gretton-dann.org.uk>
2021-01-11 19:58:19 +0000

    Add more tests for gencat

--- src/gencat/int-test-gencat.py
+++ src/gencat/int-test-gencat.py
@@ -14,7 +14,6 @@ tester.run_test(
 
 tester.run_test(
     [tester.exe(), '-', '-'], test_name="Empty input",
-    expected_rc=0,
     stdin=b'$ empty msgcat\n',
     expected_stdout=b"MSG\0\1\0\0\0\0\0\0\0\0\0\0\0\x18\0\0\0\0\0\0\0",
     expected_stderr=b'')
@@ -22,23 +21,50 @@ tester.run_test(
 # Generate a basic catalogue
 test_output = tester.output_file('test.msg')
 test_input = tester.input_file('basic-input.cat')
+test_expected = tester.input_file('basic-input.msg')
 skip_next = tester.run_test(
     [tester.exe(), test_output, test_input],
     test_name="Basic catalogue",
-    expected_rc=0,
     expected_stdout='', expected_stderr='', files=[test_output])
 tester.compare_files(
-    test_output, tester.input_file('basic-input.msg'), skip=skip_next)
+    test_output, test_expected, skip=skip_next)
 
 # Check that trying to amend it with an empty file does nothing.
 skip_next2 = tester.run_test(
     [tester.exe(), test_output, '-'],
-    test_name="Empty input doesn't modify",
-    expected_rc=0,
+    test_name="Empty input doesn't modify existing",
     stdin='$ empty msgcat\n',
     expected_stdout='', expected_stderr='', files=[test_output], skip=skip_next)
 tester.compare_files(
-    test_output, tester.input_file('basic-input.msg'), skip=skip_next2)
+    test_output, test_expected, skip=skip_next2)
 
+# Similar but not quite
+test_output3 = tester.output_file('test3.msg')
+skip_next3 = tester.run_test(
+    [tester.exe(), test_output3, test_input, '-'],
+    test_name="Empty input doesn't modify existing (new)",
+    stdin='$ empty msgcat\n',
+    expected_stdout='', expected_stderr='', files=[test_output3], skip=skip_next)
+tester.compare_files(
+    test_output3, test_expected, skip=skip_next3)
+
+# Check that trying to amend it with the same file does nothing, except produce
+# a lot of warnings.
+test_output4 = tester.output_file('test4.msg')
+skip_next4 = tester.run_test(
+    [tester.exe(), test_output4, test_input, test_input],
+    test_name="Repeated input doesn't modify (new)",
+    expected_stdout='', files=[test_output4], skip=skip_next)
+tester.compare_files(
+    test_output4, test_expected, skip=skip_next4)
+
+# Similar but not quite
+skip_next5 = tester.run_test(
+    [tester.exe(), test_output, test_input],
+    test_name="Repeated input doesn't modify existing (new)",
+    expected_rc=0,
+    expected_stdout='', files=[test_output], skip=skip_next)
+tester.compare_files(
+    test_output, test_expected, skip=skip_next5)
 
 tester.summarize()
