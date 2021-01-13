634c2e29dcfb838411d6c0cca0b2b3acffa73837
Matthew Gretton-Dann <matt@gretton-dann.org.uk>
2021-01-13 15:21:44 +0000

    Add stdlib.h wrapper
    At the moment this only needs to provide mkstemp() on Win32.

---
+++ libgdsup/include/gd/stdlib.h
@@ -0,0 +1,21 @@
+/** \file   libgdsup/include/gd/stdlib.h
+ *  \brief  Expose stdlib.h
+ *  \author Copyright 2021, Matthew Gretton-Dann
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _LIBGDSUP_INCLUDE_GD_STDLIB_H_INCLUDED
+#define _LIBGDSUP_INCLUDE_GD_STDLIB_H_INCLUDED
+
+/* Everyone has a stdlib.h.  */
+#include <stdlib.h>
+
+#if _WIN32
+/** \brief       Create a unique file
+ *  \param  temp Template for filename.  Must end in six 'X' characters.
+ *  \return      Open file descriptor, or -1 on error.
+ */
+int mkstemp(char *temp);
+#endif // _WIN32
+
+#endif // _LIBGDSUP_INCLUDE_GD_STDLIB_H_INCLUDED
---
+++ libgdsup/stdlib/mkstemp-win32.c
@@ -0,0 +1,44 @@
+/** \file   libgdsup/stdlib/mkstemp-win32.c
+ *  \brief  Win32 API implementation of mkstemp()
+ *  \author Copyright 2021, Matthew Gretton-Dann
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "gd/stdlib.h"
+
+#define _CRT_DECLARE_NONSTDC_NAMES 0
+#include <errno.h>
+#include <fcntl.h>
+#include <io.h>
+#include <share.h>
+#include <stddef.h>
+#include <string.h>
+#include <sys/stat.h>
+
+int mkstemp(char* temp)
+{
+  /* Find length of template.  */
+  if (temp == NULL) {
+    errno = ENOENT;
+    return -1;
+  }
+  size_t len = strlen(temp);
+
+  /* Repeat until we manage to open a file, or have an error that isn't "file exists". */
+  while (1) {
+    errno_t err = _mktemp_s(temp, len + 1);
+    if (err != 0) {
+      errno = err;
+      return -1;
+    }
+
+    int fd;
+    err = _sopen_s(&fd, temp, _O_RDWR | _O_CREAT | _O_EXCL, _SH_DENYNO, _S_IREAD | _S_IWRITE);
+    if (fd != -1 || err != EEXIST) {
+      if (fd == -1) {
+        errno = err;
+      }
+      return fd;
+    }
+  }
+}
