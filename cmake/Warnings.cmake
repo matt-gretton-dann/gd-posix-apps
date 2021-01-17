4ddac742d5e230792047a4d2295b7bde3523cf9d
Matthew Gretton-Dann <matt@gretton-dann.org.uk>
2021-01-17 17:48:10 +0000

    Add -Wall -Werror equivalent to buids

---
+++ cmake/Warnings.cmake
@@ -0,0 +1,16 @@
+# \file   cmake/Warnings.cmake
+# \brief  Add -Wall -Werror (or equivalent) on a target
+# \author Copyright 2020, Matthew Gretton-Dann
+#         SPDX-License-Identifier: Apache-2.0
+
+# Enable all warnings and treat them as errors on the given target.
+# Is in theory cross-platform.
+function(set_warnings TARGET)
+
+if(MSVC)
+target_compile_options(${TARGET} PRIVATE /Wall /WX)
+else()
+target_compile_options(${TARGET} PRIVATE -Wall -Wextra -Wpedantic -Werror)
+endif()
+
+endfunction()
