9300e579af414cca9889848897c63f76fbb83a00
Matthew Gretton-Dann <matt@gretton-dann.org.uk>
2021-02-03 15:22:58 +0000

    Enable expected output to be regular expression.

--- src/basename/int-test-basename.py
+++ src/basename/int-test-basename.py
@@ -3,7 +3,7 @@
 # Copyright 2021, Matthew Gretton-Dann
 # SPDX-License-Identifier: Apache-2.0
 
-
+import re
 import sys
 import int_tests
 
@@ -14,11 +14,11 @@ tester.run_test(
 tester.run_test(
     [tester.exe(), "usr/"], test_name="usr/", expected_stdout="usr\n")
 tester.run_test(
-    [tester.exe(), ""], test_name="\"\"", expected_stdout=".\n")
+    [tester.exe(), ""], test_name="\"\"", expected_stdout=re.compile("^.?\n$"))
 tester.run_test(
     [tester.exe(), "/"], test_name="/", expected_stdout="/\n")
 tester.run_test(
-    [tester.exe(), "//"], test_name="//", expected_stdout="//\n")
+    [tester.exe(), "//"], test_name="//", expected_stdout=re.compile("^//?\n$"))
 tester.run_test(
     [tester.exe(), "///"], test_name="///", expected_stdout="/\n")
 tester.run_test(
@@ -36,8 +36,8 @@ tester.run_test(
     [tester.exe(), "/home/fred/file.txt", ".sh"], test_name="/home/fred/file.txt .sh", expected_stdout="file.txt\n")
 
 tester.run_test(
-    [tester.exe()], test_name="Empty command line", expected_rc=1, expected_stdout="")
+    [tester.exe()], test_name="Empty command line", expected_rc=1, expected_stdout="", expected_stderr=re.compile(r"^(.*basename(\.exe)?): Missing arguments, expect at least a path\.\nUsage: \1 string \[suffix\]\n$"))
 tester.run_test(
-    [tester.exe(), "a", "b", "c"], test_name="Long command line", expected_rc=1, expected_stdout="")
+    [tester.exe(), "a", "b", "c"], test_name="Long command line", expected_rc=1, expected_stdout="", expected_stderr=re.compile(r"^(.*basename(\.exe)?): Too many arguments have been specified\.\nUsage: \1 string \[suffix\]\n$"))
 
 tester.summarize()
