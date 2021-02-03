8b803ea5b1157cd137c95dfad79bc7869ae79422
Matthew Gretton-Dann <matt@gretton-dann.org.uk>
2021-02-03 12:48:13 +0000

    Add a utility library for src/
    This is different from libgdsup as it does non-standard functionality.

---
+++ src/util/test-util-main.cc
@@ -0,0 +1,15 @@
+/** \file   libgdsup/support/test-util-main.cc
+ *  \brief  Main file for gdsup unit tests
+ *  \author Copyright 2021, Matthew Gretton-Dann
+ *          SPDX-License-Identifier: Apache-2.0
+ */
+
+#define CATCH_CONFIG_RUNNER
+#include <catch2/catch.hpp>
+
+int main(int argc, char* argv[])
+{
+  int result = Catch::Session().run(argc, argv);
+
+  return result;
+}
