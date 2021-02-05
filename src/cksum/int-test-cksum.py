37d7807c1b2e4aebf3adabfc1ecb529436aa3264
Matthew Gretton-Dann <matt@gretton-dann.org.uk>
2021-02-05 18:49:51 +0000

    Fix Windows tests

--- src/cksum/int-test-cksum.py
+++ src/cksum/int-test-cksum.py
@@ -12,21 +12,21 @@ tester = int_tests.TestRunner()
 # Test input translation via std
 tester.run_test(
     [tester.exe()],
-    stdin=b"",
-    expected_stdout=b"4294967295 0\n",
-    expected_stderr=b"")
+    stdin="",
+    expected_stdout="4294967295 0\n",
+    expected_stderr="")
 tester.run_test(
     [tester.exe(), "-"],
-    stdin=b"\0",
-    expected_stdout=b"4215202376 1\n",
-    expected_stderr=b"")
+    stdin="\0",
+    expected_stdout="4215202376 1\n",
+    expected_stderr="")
 
 input1 = tester.input_file("test-input1.txt")
 input2 = tester.input_file("test-input2.txt")
 expected_output = f"1655064767 1355 {input1}\n1533133176 681 {input2}\n"
 tester.run_test(
     [tester.exe(), input1, input2],
-    expected_stdout=expected_output.encode())
+    expected_stdout=expected_output)
 
 # Test bad options
 tester.run_test(
