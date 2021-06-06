3549165e39de5de6b91f350ab0b083797281219c
Matthew Gretton-Dann <matt@gretton-dann.org.uk>
2021-06-06 20:46:31 +0100

    Add support for expected fails
    All the bc tests from gavinhoward-bc currently fail because we don't
    implement all the necessary extensions.

--- cmake/GDPosixAppsTests.cmake
+++ cmake/GDPosixAppsTests.cmake
@@ -26,25 +26,60 @@ else()
 endif()
 
 
-function(add_gdpat_test UTILITY)
+function(add_gdpat_test)
+  set(_agt_options "")
+  set(_agt_single_options NAME)
+  set(_agt_multi_options XFAILS SKIPS)
+  cmake_parse_arguments(_agt
+                        "${_agt_options}"
+                        "${_agt_single_options}"
+                        "${_agt_multi_options}"
+                        ${ARGN})
+  if(NOT DEFINED _agt_NAME)
+    message(FATAL_ERROR "Must specify NAME to add_gdpat_test")
+  endif()
+
   if(NOT USE_GD_POSIX_APPS_TESTS OR NOT BUILD_TESTING)
     message(STATUS "Not using gd-posix-app-tests for ${UTILITY}")
     return()
   endif()
 
+
   execute_process(
-    COMMAND "${Python_EXECUTABLE}" "${gdpat_SOURCE_DIR}/run-tests.py" "--list-tests" "${UTILITY}"
+    COMMAND "${Python_EXECUTABLE}" "${gdpat_SOURCE_DIR}/run-tests.py" "--list-tests" "${_agt_NAME}"
     OUTPUT_VARIABLE _agt_TESTS)
   # Newline in next command is deliberate do not remove
   string(REPLACE "
 " ";" _agt_TESTS "${_agt_TESTS}")
+
   foreach(_agt_TEST IN LISTS _agt_TESTS)
-    if (_agt_TEST)
+    if(_agt_TEST)
+      if(DEFINED _agt_SKIPS)
+        set(_agt_SKIP_TEST OFF)
+        foreach(_agt_SKIP IN LISTS _agt_SKIPS)
+          if("${_agt_TEST}" MATCHES "^${_agt_SKIP}[/$]")
+            message(STATUS "Skipping ${_agt_TEST} as it matches skip: ${_agt_SKIP}")
+            set(_agt_SKIP_TEST ON)
+            break()
+          endif()
+        endforeach()
+        if(_agt_SKIP_TEST)
+          continue()
+        endif()
+      endif()
+      set(_agt_XFAIL_OPTS)
       add_test(
         NAME "${_agt_TEST}"
         COMMAND "${Python_EXECUTABLE}" "${gdpat_SOURCE_DIR}/run-tests.py"
-                "--utility" "$<TARGET_FILE:${UTILITY}>"
-                "${_agt_TEST}")
+                "--utility" "$<TARGET_FILE:${_agt_NAME}>"
+                "${_agt_TEST}" ${_agt_XFAIL_OPTS})
+      if(DEFINED _agt_XFAILS)
+        foreach(_agt_XFAIL IN LISTS _agt_XFAILS)
+          if("${_agt_TEST}" MATCHES "^${_agt_XFAIL}[/$]")
+            set_tests_properties("${_agt_TEST}" PROPERTIES WILL_FAIL ON)
+          endif()
+        endforeach()
+      endif()
     endif()
   endforeach()
 endfunction()
