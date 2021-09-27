05714320367ba65f247b5fa37ada13bbc6e076e8
Matthew Gretton-Dann <matt@gretton-dann.org.uk>
2021-09-27 15:17:49 +0100

    Update tests

--- src/ar/int-test-ar.py
+++ src/ar/int-test-ar.py
@@ -5,8 +5,9 @@
 
 
 import filecmp
-import sys
 import int_tests
+import re
+import sys
 
 tester = int_tests.TestRunner()
 
@@ -17,23 +18,31 @@ test_input1 = tester.input_file('input1.txt')
 test_input2 = tester.input_file('input2.txt')
 test_ar1 = tester.output_file('ar1.ar')
 test_ar2 = tester.output_file('ar2.ar')
+test_ar3 = tester.output_file('ar3.ar')
 
 # Create an archive twice to see if the different ways of generating produce the same results.
 failed_ar1_creation = tester.run_test(
     [tester.exe(), "-rc", test_ar1, test_input1, test_input2],
     test_name="Quiet generate basic archive",
-    expected_rc=0, expected_stdout='')
+    expected_rc=0, expected_stdout="", expected_stderr="")
 failed_ar2_creation = tester.run_test(
-    [tester.exe(), "-r", test_ar1, test_input1, test_input2],
+    [tester.exe(), "-r", test_ar2, test_input1, test_input2],
     test_name="generate basic archive",
-    expected_rc=0, expected_stdout='.*ar2\\.ar.*')
+    expected_rc=0, expected_stderr=re.compile(f'.*{re.escape(test_ar2)}.*'))
+failed_ar3_creation = tester.run_test(
+    [tester.exe(), "-rcv", test_ar3, test_input1, test_input2],
+    test_name="Verbose generate basic archive",
+    expected_rc=0, expected_stdout=f"a - {test_input1}\na - {test_input2}\n")
 
-tester.compare_files(test_ar1, test_ar2, failed_ar1_creation or failed_ar2_creation)
+tester.compare_files(test_ar1, test_ar2,
+                     failed_ar1_creation or failed_ar2_creation)
+tester.compare_files(test_ar1, test_ar3,
+                     failed_ar1_creation or failed_ar3_creation)
 
 # Check the contents of the archive
 tester.run_test(
     [tester.exe(), "-t", test_ar1],
     test_name="ar1 ToC",
-    expected_rc=0, expected_stdout="ar1.ar\nar2.ar", expected_stderr="")
+    expected_rc=0, expected_stdout="input1.txt\ninput2.txt\n", expected_stderr="")
 
 tester.summarize()
