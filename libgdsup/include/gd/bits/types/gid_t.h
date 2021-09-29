174d06bc63e85abe7ef063623bceecc18d8efa78
Matthew Gretton-Dann <matt@gretton-dann.org.uk>
2021-09-29 15:50:02 +0100

    Add some more Windows specific types.
    These are the ones required for stat.

--- libgdsup/include/gd/bits/types/gid_t.h
+++ libgdsup/include/gd/bits/types/gid_t.h
@@ -1,15 +1,15 @@
-/** \file   libgdsup/include/bits/types/gid_t.h
+/** \file   libgdsup/include/bits/types/uid_t.h
  *  \brief  Define the pid_t type.
  *  \author Copyright 2021, Matthew Gretton-Dann
  *  SPDX-License-Identifier: Apache-2.0
  */
 
-#ifndef _LIBGDSUP_INCLUDE_BITS_TYPES_UID_T_H_INCLUDED
-#define _LIBGDSUP_INCLUDE_BITS_TYPES_UID_T_H_INCLUDED
+#ifndef _LIBGDSUP_INCLUDE_BITS_TYPES_GID_T_H_INCLUDED
+#define _LIBGDSUP_INCLUDE_BITS_TYPES_GID_T_H_INCLUDED
 
 #ifdef _WIN32
 /** On Windows we map group IDs to intptrs so we can handle them as Pointers too.  */
-typedef intptr_t uid_t;
+typedef intptr_t gid_t;
 #endif
 
 #endif  // _LIBGDSUP_INCLUDE_BITS_TYPES_UID_T_H_INCLUDED
