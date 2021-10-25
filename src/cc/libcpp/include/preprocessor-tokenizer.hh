8f1908d83fa8868b8cbc7ddf68f52c1fc297aac6
Matthew Gretton-Dann <matt@gretton-dann.org.uk>
2021-10-25 08:59:50 +0100

    Add parsing of identifiers.
    This doesn't yet deal with an identifier that starts with a \u or \U
    sequence.

--- src/cc/libcpp/include/preprocessor-tokenizer.hh
+++ src/cc/libcpp/include/preprocessor-tokenizer.hh
@@ -10,15 +10,91 @@
 
 #include "error.hh"
 #include "file-store.hh"
+#include "identifier-manager.hh"
 #include "token.hh"
 #include "tokenizer.hh"
 
 namespace GD::CPP {
 
 namespace Details {
+/** \brief  Is \a c classified as a whitespace character? */
 constexpr inline auto is_whitespace(char32_t c) -> bool
 {
   return c == U' ' || c == U'\t' || c == U'\f' || c == U'\v';
+}
+
+/** \brief  Is this a valid Universal Character name identifier?  */
+constexpr inline auto is_ucn_identifier(char32_t c) -> bool
+{
+  return c == U'\u00A8' || c == U'\u00AA' || c == U'\u00AD' || c == U'\u00AF' ||
+         (c >= U'\u00B2' && c <= U'\u00B5') || (c >= U'\u00B7' && c <= U'\u00BA') ||
+         (c >= U'\u00BC' && c <= U'\u00BE') || (c >= U'\u00C0' && c <= U'\u00D6') ||
+         (c >= U'\u00D8' && c <= U'\u00F6') || (c >= U'\u00F8' && c <= U'\u00FF') ||
+         (c >= U'\u0100' && c <= U'\u167F') || (c >= U'\u1681' && c <= U'\u180D') ||
+         (c >= U'\u180F' && c <= U'\u1FFF') || (c >= U'\u200B' && c <= U'\u200D') ||
+         (c >= U'\u202A' && c <= U'\u202E') || (c >= U'\u203F' && c <= U'\u2040') ||
+         c == U'\u2054' || (c >= U'\u2060' && c <= U'\u206F') ||
+         (c >= U'\u2070' && c <= U'\u218F') || (c >= U'\u2460' && c <= U'\u24FF') ||
+         (c >= U'\u2776' && c <= U'\u2793') || (c >= U'\u2C00' && c <= U'\u2DFF') ||
+         (c >= U'\u2E80' && c <= U'\u2FFF') || (c >= U'\u3004' && c <= U'\u3007') ||
+         (c >= U'\u3021' && c <= U'\u302F') || (c >= U'\u3031' && c <= U'\u303F') ||
+         (c >= U'\u3040' && c <= U'\uD7FF') || (c >= U'\uF900' && c <= U'\uFD3D') ||
+         (c >= U'\uFD40' && c <= U'\uFDCF') || (c >= U'\uFDF0' && c <= U'\uFE44') ||
+         (c >= U'\uFE47' && c <= U'\uFFFD') || (c >= U'\U00010000' && c <= U'\U0001FFFD') ||
+         (c >= U'\U00020000' && c <= U'\U0002FFFD') || (c >= U'\U00030000' && c <= U'\U0003FFFD') ||
+         (c >= U'\U00040000' && c <= U'\U0004FFFD') || (c >= U'\U00050000' && c <= U'\U0005FFFD') ||
+         (c >= U'\U00060000' && c <= U'\U0006FFFD') || (c >= U'\U00070000' && c <= U'\U0007FFFD') ||
+         (c >= U'\U00080000' && c <= U'\U0008FFFD') || (c >= U'\U00090000' && c <= U'\U0009FFFD') ||
+         (c >= U'\U000A0000' && c <= U'\U000AFFFD') || (c >= U'\U000B0000' && c <= U'\U000BFFFD') ||
+         (c >= U'\U000C0000' && c <= U'\U000CFFFD') || (c >= U'\U000D0000' && c <= U'\U000DFFFD') ||
+         (c >= U'\U000E0000' && c <= U'\U000EFFFD');
+}
+
+/** \brief  Is this a valid Universal Character name initial identifier?  */
+constexpr inline auto is_ucn_initial_identifier(char32_t c) -> bool
+{
+  bool is_id = is_ucn_identifier(c);
+  bool disallowed_first = (c >= U'\u0300' && c <= U'\u036F') ||
+                          (c >= U'\u1DC0' && c <= U'\u1DFF') ||
+                          (c >= U'\u20D0' && c <= U'\u20FF') || (c >= U'\uFE20' && c <= U'\uFE2F');
+  return is_id && !disallowed_first;
+}
+
+/** \brief Is this a non-digit?  */
+constexpr inline auto is_nondigit(char32_t c) -> bool
+{
+  /* This range check is safe because char32_t is a Unicode encoded set where A-Z & a-z are
+   * contiguous.  */
+  return c == '_' || (c >= U'A' && c <= U'Z') || (c >= U'a' && c <= u'z');
+}
+
+/** \brief Is this a digit?  */
+constexpr inline auto is_digit(char32_t c) -> bool
+{
+  /* This range check is safe because char32_t is a Unicode encoded set where A-Z & a-z are
+   * contiguous.  */
+  return c >= '0' && c <= '9';
+}
+
+constexpr inline auto is_hex_digit(char32_t c) -> bool
+{
+  return (c >= '0' && c <= '9') || (c >= 'A' && c <= 'F') || (c >= 'a' && c <= 'f');
+}
+
+constexpr inline auto hex_digit_value(char32_t c) -> unsigned
+{
+  constexpr unsigned offset = 10;
+  if (c >= '0' && c <= '9') {
+    return c - '0';
+  }
+  if (c >= 'A' && c <= 'F') {
+    return c - 'A' + offset;
+  }
+  if (c >= 'a' && c <= 'f') {
+    return c - 'a' + offset;
+  }
+
+  return std::numeric_limits<unsigned>::max();
 }
 
 }  // namespace Details
@@ -34,8 +110,9 @@ template<typename Parent>
 class PreprocessorTokenizer : public Tokenizer<PreprocessorTokenizer<Parent>, Parent>
 {
 public:
-  PreprocessorTokenizer(Parent& parent, ErrorManager& em)
-      : Tokenizer<PreprocessorTokenizer<Parent>, Parent>(parent), error_manager_(em)
+  PreprocessorTokenizer(Parent& parent, ErrorManager& em, IdentifierManager& id)
+      : Tokenizer<PreprocessorTokenizer<Parent>, Parent>(parent), error_manager_(em),
+        identifier_manager_(id)
   {
   }
 
@@ -176,6 +253,76 @@ private:
     return {TokenType::white_space, Range{begin, end}};
   }
 
+  auto read_char32_hex_digits(Parent& parent, unsigned count) -> char32_t
+  {
+    assert_ice(count > 0 && count < 9, "Can only read up to 8 hex digits.");
+    auto begin = parent.peek().range().begin();
+    char32_t result = 0;
+    for (unsigned i = 0; i < count; ++i) {
+      auto const& t = parent.peek();
+      if (t != TokenType::character || !Details::is_hex_digit(t.character())) {
+        error_manager_.error(ErrorCode::not_enough_hex_digits, Range{begin, t.range().end()}, count,
+                             i);
+        return U'\ufffd';
+      }
+
+      result *= 16;  // NOLINT
+      result += Details::hex_digit_value(t.character());
+      parent.chew();
+    }
+
+    return result;
+  }
+
+  auto parse_identifier(Parent& parent) -> Token
+  {
+    std::u32string id{};
+    auto begin = parent.peek().range().begin();
+    auto end = begin;
+    while (true) {
+      auto const& t = parent.peek();
+      if (t != TokenType::character) {
+        break;
+      }
+      char32_t c = t.character();
+      if (Details::is_ucn_identifier(c)) {
+        error_manager_.error(ErrorCode::explicit_ucn_implementation_defined, t.range(),
+                             to_string(c));
+        parent.chew();
+      }
+      else if (c == U'\\') {
+        Token back_slash{t};
+        parent.chew();
+        if (parent.peek() != U'U' && parent.peek() != U'u') {
+          pending_ = back_slash;
+          break;
+        }
+
+        constexpr unsigned Ulen = 8;
+        constexpr unsigned ulen = 4;
+        unsigned count = parent.peek() == U'U' ? Ulen : ulen;
+        parent.chew();
+        c = read_char32_hex_digits(parent, count);
+        if (!Details::is_ucn_identifier(c)) {
+          pending_ = Token{TokenType::character,
+                           Range{back_slash.range().begin(), parent.peek().range().begin()}, c};
+          break;
+        }
+      }
+      else if (Details::is_digit(c) || Details::is_nondigit(c)) {
+        parent.chew();
+      }
+      else {
+        break;
+      }
+
+      id.push_back(c);
+      end = t.range().end();
+    }
+
+    return Token{TokenType::identifier, Range{begin, end}, identifier_manager_.id(id)};
+  }
+
   auto do_peek(Parent& parent) -> std::optional<Token>
   {
     if (pending_) {
@@ -191,11 +338,15 @@ private:
       if (Details::is_whitespace(c) || c == U'/') {
         return parse_whitespace_and_slash(parent);
       }
+      if (Details::is_nondigit(c) || Details::is_ucn_initial_identifier(c)) {
+        return parse_identifier(parent);
+      }
     }
     return std::nullopt;
   }
 
   ErrorManager& error_manager_;                 ///< Error manager
+  IdentifierManager& identifier_manager_;       ///< Identifier manager
   std::optional<Token> pending_{std::nullopt};  ///< Pending token
 };
 
--- src/cc/libcpp/test-preprocessor-tokenizer.cc
+++ src/cc/libcpp/test-preprocessor-tokenizer.cc
@@ -12,6 +12,7 @@
 
 #include "error.hh"
 #include "file-store.hh"
+#include "identifier-manager.hh"
 #include "location.hh"
 #include "preprocessor-tokenizer.hh"
 
@@ -19,44 +20,50 @@ TEST_CASE("GD::CPP::PreprocessorTokenizer - Whitespace", "[cpp][preprocessor-tok
 {
   std::ostringstream os;
   GD::CPP::ErrorManager error_manager(os);
+  GD::CPP::IdentifierManager id_manager;
   GD::CPP::FileStore file_store(error_manager);
   error_manager.file_store(file_store);
-  auto tokenizer = GD::CPP::PreprocessorTokenizer(file_store, error_manager);
+  auto tokenizer = GD::CPP::PreprocessorTokenizer(file_store, error_manager, id_manager);
 
   auto fname = std::string("Test");
   auto input = std::string("a \t \f \v b");
   auto is = std::istringstream(input);
   tokenizer.push_stream(fname, is);
 
-  REQUIRE(tokenizer.peek() == U'a');
+  REQUIRE(tokenizer.peek() == GD::CPP::TokenType::identifier);
+  REQUIRE(id_manager.display_name(tokenizer.peek().identifier()) == "a");
   tokenizer.chew();
   REQUIRE(tokenizer.peek() == GD::CPP::TokenType::white_space);
   tokenizer.chew();
-  REQUIRE(tokenizer.peek() == U'b');
+  REQUIRE(tokenizer.peek() == GD::CPP::TokenType::identifier);
+  REQUIRE(id_manager.display_name(tokenizer.peek().identifier()) == "b");
   tokenizer.chew();
   REQUIRE(tokenizer.peek() == GD::CPP::TokenType::end_of_include);
   tokenizer.chew();
   REQUIRE(tokenizer.peek() == GD::CPP::TokenType::end_of_source);
 }
 
-TEST_CASE("GD::CPP::PreprocessorTokenizer - multi-line comment", "[cpp][comment-chewer]")
+TEST_CASE("GD::CPP::PreprocessorTokenizer - multi-line comment", "[cpp][preprocessor-tokenizer]")
 {
   std::ostringstream os;
   GD::CPP::ErrorManager error_manager(os);
+  GD::CPP::IdentifierManager id_manager;
   GD::CPP::FileStore file_store(error_manager);
   error_manager.file_store(file_store);
-  auto tokenizer = GD::CPP::PreprocessorTokenizer(file_store, error_manager);
+  auto tokenizer = GD::CPP::PreprocessorTokenizer(file_store, error_manager, id_manager);
 
   auto fname = std::string("Test");
   auto input = std::string("a/**/b");
   auto is = std::istringstream(input);
   tokenizer.push_stream(fname, is);
 
-  REQUIRE(tokenizer.peek() == U'a');
+  REQUIRE(tokenizer.peek() == GD::CPP::TokenType::identifier);
+  REQUIRE(id_manager.display_name(tokenizer.peek().identifier()) == "a");
   tokenizer.chew();
   REQUIRE(tokenizer.peek() == GD::CPP::TokenType::white_space);
   tokenizer.chew();
-  REQUIRE(tokenizer.peek() == U'b');
+  REQUIRE(tokenizer.peek() == GD::CPP::TokenType::identifier);
+  REQUIRE(id_manager.display_name(tokenizer.peek().identifier()) == "b");
   tokenizer.chew();
   REQUIRE(tokenizer.peek() == GD::CPP::TokenType::end_of_include);
   tokenizer.chew();
@@ -64,26 +71,30 @@ TEST_CASE("GD::CPP::PreprocessorTokenizer - multi-line comment", "[cpp][comment-
   REQUIRE(os.str().empty());
 }
 
-TEST_CASE("GD::CPP::PreprocessorTokenizer - line comment", "[cpp][comment-chewer]")
+TEST_CASE("GD::CPP::PreprocessorTokenizer - line comment", "[cpp][preprocessor-tokenizer]")
 {
   std::ostringstream os;
   GD::CPP::ErrorManager error_manager(os);
+  GD::CPP::IdentifierManager id_manager;
+
   GD::CPP::FileStore file_store(error_manager);
   error_manager.file_store(file_store);
-  auto tokenizer = GD::CPP::PreprocessorTokenizer(file_store, error_manager);
+  auto tokenizer = GD::CPP::PreprocessorTokenizer(file_store, error_manager, id_manager);
 
   auto fname = std::string("Test");
   auto input = std::string("a// b\nc");
   auto is = std::istringstream(input);
   tokenizer.push_stream(fname, is);
 
-  REQUIRE(tokenizer.peek() == U'a');
+  REQUIRE(tokenizer.peek() == GD::CPP::TokenType::identifier);
+  REQUIRE(id_manager.display_name(tokenizer.peek().identifier()) == "a");
   tokenizer.chew();
   REQUIRE(tokenizer.peek() == GD::CPP::TokenType::white_space);
   tokenizer.chew();
   REQUIRE(tokenizer.peek() == U'\n');
   tokenizer.chew();
-  REQUIRE(tokenizer.peek() == U'c');
+  REQUIRE(tokenizer.peek() == GD::CPP::TokenType::identifier);
+  REQUIRE(id_manager.display_name(tokenizer.peek().identifier()) == "c");
   tokenizer.chew();
   REQUIRE(tokenizer.peek() == GD::CPP::TokenType::end_of_include);
   tokenizer.chew();
@@ -92,20 +103,22 @@ TEST_CASE("GD::CPP::PreprocessorTokenizer - line comment", "[cpp][comment-chewer
 }
 
 TEST_CASE("GD::CPP::PreprocessorTokenizer - unterminated multi-line comment",
-          "[cpp][comment-chewer]")
+          "[cpp][preprocessor-tokenizer]")
 {
   std::ostringstream os;
   GD::CPP::ErrorManager error_manager(os);
+  GD::CPP::IdentifierManager id_manager;
   GD::CPP::FileStore file_store(error_manager);
   error_manager.file_store(file_store);
-  auto tokenizer = GD::CPP::PreprocessorTokenizer(file_store, error_manager);
+  auto tokenizer = GD::CPP::PreprocessorTokenizer(file_store, error_manager, id_manager);
 
   auto fname = std::string("Test");
   auto input = std::string("a/**b");
   auto is = std::istringstream(input);
   tokenizer.push_stream(fname, is);
 
-  REQUIRE(tokenizer.peek() == U'a');
+  REQUIRE(tokenizer.peek() == GD::CPP::TokenType::identifier);
+  REQUIRE(id_manager.display_name(tokenizer.peek().identifier()) == "a");
   tokenizer.chew();
   REQUIRE(os.str().empty());
   REQUIRE(tokenizer.peek() == GD::CPP::TokenType::white_space);
@@ -118,26 +131,106 @@ TEST_CASE("GD::CPP::PreprocessorTokenizer - unterminated multi-line comment",
   REQUIRE(errs == os.str());
 }
 
-TEST_CASE("GD::CPP::PreprocessorTokenizer - unterminated line comment", "[cpp][comment-chewer]")
+TEST_CASE("GD::CPP::PreprocessorTokenizer - unterminated line comment",
+          "[cpp][preprocessor-tokenizer]")
 {
   std::ostringstream os;
   GD::CPP::ErrorManager error_manager(os);
+  GD::CPP::IdentifierManager id_manager;
   GD::CPP::FileStore file_store(error_manager);
   error_manager.file_store(file_store);
-  auto tokenizer = GD::CPP::PreprocessorTokenizer(file_store, error_manager);
+  auto tokenizer = GD::CPP::PreprocessorTokenizer(file_store, error_manager, id_manager);
 
   auto fname = std::string("Test");
   auto input = std::string("a// bc");
   auto is = std::istringstream(input);
   tokenizer.push_stream(fname, is);
 
-  REQUIRE(tokenizer.peek() == U'a');
+  REQUIRE(tokenizer.peek() == GD::CPP::TokenType::identifier);
+  REQUIRE(id_manager.display_name(tokenizer.peek().identifier()) == "a");
   tokenizer.chew();
   REQUIRE(os.str().empty());
   REQUIRE(tokenizer.peek() == GD::CPP::TokenType::white_space);
   tokenizer.chew();
   auto errs{os.str()};
   REQUIRE(!errs.empty());
+  REQUIRE(tokenizer.peek() == GD::CPP::TokenType::end_of_include);
+  tokenizer.chew();
+  REQUIRE(tokenizer.peek() == GD::CPP::TokenType::end_of_source);
+  REQUIRE(errs == os.str());
+}
+
+TEST_CASE("GD::CPP::PreprocessorTokenizer - identifier big literal",
+          "[cpp][preprocessor-tokenizer]")
+{
+  std::ostringstream os;
+  GD::CPP::ErrorManager error_manager(os);
+  GD::CPP::IdentifierManager id_manager;
+  GD::CPP::FileStore file_store(error_manager);
+  error_manager.file_store(file_store);
+  auto tokenizer = GD::CPP::PreprocessorTokenizer(file_store, error_manager, id_manager);
+
+  auto fname = std::string("Test");
+  auto input = std::string("a\xF0\x9F\x98\x81z1");
+  auto is = std::istringstream(input);
+  tokenizer.push_stream(fname, is);
+
+  REQUIRE(os.str().empty());
+  REQUIRE(tokenizer.peek() == GD::CPP::TokenType::identifier);
+  auto errs{os.str()};
+  REQUIRE(!errs.empty());
+  REQUIRE(id_manager.display_name(tokenizer.peek().identifier()) == "a\\U0001f601z1");
+  tokenizer.chew();
+  REQUIRE(tokenizer.peek() == GD::CPP::TokenType::end_of_include);
+  tokenizer.chew();
+  REQUIRE(tokenizer.peek() == GD::CPP::TokenType::end_of_source);
+  REQUIRE(os.str() == errs);
+}
+
+TEST_CASE("GD::CPP::PreprocessorTokenizer - identifier UCN", "[cpp][preprocessor-tokenizer]")
+{
+  std::ostringstream os;
+  GD::CPP::ErrorManager error_manager(os);
+  GD::CPP::IdentifierManager id_manager;
+  GD::CPP::FileStore file_store(error_manager);
+  error_manager.file_store(file_store);
+  auto tokenizer = GD::CPP::PreprocessorTokenizer(file_store, error_manager, id_manager);
+
+  auto fname = std::string("Test");
+  auto input = std::string("a\\U0001f601z");
+  auto is = std::istringstream(input);
+  tokenizer.push_stream(fname, is);
+
+  REQUIRE(tokenizer.peek() == GD::CPP::TokenType::identifier);
+  REQUIRE(id_manager.display_name(tokenizer.peek().identifier()) == "a\\U0001f601z");
+  tokenizer.chew();
+  REQUIRE(tokenizer.peek() == GD::CPP::TokenType::end_of_include);
+  tokenizer.chew();
+  REQUIRE(tokenizer.peek() == GD::CPP::TokenType::end_of_source);
+  REQUIRE(os.str().empty());
+}
+
+TEST_CASE("GD::CPP::PreprocessorTokenizer - identifier UCN not enough hex",
+          "[cpp][preprocessor-tokenizer]")
+{
+  std::ostringstream os;
+  GD::CPP::ErrorManager error_manager(os);
+  GD::CPP::IdentifierManager id_manager;
+  GD::CPP::FileStore file_store(error_manager);
+  error_manager.file_store(file_store);
+  auto tokenizer = GD::CPP::PreprocessorTokenizer(file_store, error_manager, id_manager);
+
+  auto fname = std::string("Test");
+  auto input = std::string("a\\U0001f60z");
+  auto is = std::istringstream(input);
+  tokenizer.push_stream(fname, is);
+
+  REQUIRE(os.str().empty());
+  REQUIRE(tokenizer.peek() == GD::CPP::TokenType::identifier);
+  auto errs{os.str()};
+  REQUIRE(!errs.empty());
+  REQUIRE(id_manager.display_name(tokenizer.peek().identifier()) == "a\\ufffdz");
+  tokenizer.chew();
   REQUIRE(tokenizer.peek() == GD::CPP::TokenType::end_of_include);
   tokenizer.chew();
   REQUIRE(tokenizer.peek() == GD::CPP::TokenType::end_of_source);
