6b5e01615befb9a91583f46951e97b84fdfb11cb
Matthew Gretton-Dann <matt@gretton-dann.org.uk>
2022-12-25 11:43:07 +0000

    Tidy files for asa.

--- src/asa/asa.cc
+++ src/asa/asa.cc
@@ -12,6 +12,8 @@
 #include "util/file.hh"
 #include "util/utils.hh"
 
+#include "asa-messages.hh"
+
 #include <cassert>
 #include <clocale>
 #include <fstream>
@@ -19,6 +21,24 @@
 #include <string>
 #include <string_view>
 #include <system_error>
+
+using Msg = GD::Asa::Msg;
+
+namespace {
+/** \brief       Report an error and exit with exit code 1.
+ *  \param  msg  Message ID
+ *  \param  args Arguments for the message.
+ */
+template<typename... Ts>
+[[noreturn]] void error(Msg msg, Ts... args)
+{
+  constexpr auto usage = Msg::usage;
+  std::cerr << GD::program_name() << ": "
+            << GD::Asa::Messages::get().format(GD::Asa::Set::asa, msg, args...) << '\n'
+            << GD::Asa::Messages::get().format(GD::Asa::Set::asa, usage, GD::program_name())
+            << '\n';
+  std::exit(1);  // NOLINT(concurrency-mt-unsafe)
+}
 
 class State
 {
@@ -64,12 +84,13 @@ public:
 private:
   bool first_input_{true};
 };
+}  // namespace
 
 auto main(int argc, char** argv) -> int
-{
+try {
   // NOLINTNEXTLINE(concurrency-mt-unsafe)
-  std::setlocale(LC_ALL, "");
-  GD::Span::span<char*> args(argv, argc);
+  (void)std::setlocale(LC_ALL, "");
+  GD::Span::span<char*> const args(argv, argc);
   GD::program_name(args[0]);
 
   State state;
@@ -81,10 +102,11 @@ auto main(int argc, char** argv) -> int
   }
 
   try {
-    bool success = GD::for_each_file(begin, args.end(), [&state, &fname](std::string_view file) {
-      fname = file;
-      return state.output(file);
-    });
+    bool const success =
+      GD::for_each_file(begin, args.end(), [&state, &fname](std::string_view file) {
+        fname = file;
+        return state.output(file);
+      });
     state.output_term();
     return success ? EXIT_SUCCESS : EXIT_FAILURE;
   }
@@ -97,4 +119,10 @@ auto main(int argc, char** argv) -> int
     std::cerr << ": " << e.what() << "\n";
     return EXIT_FAILURE;
   }
+}
+catch (std::exception& e) {
+  error(Msg::uncaught_std_exception, e.what());
+}
+catch (...) {
+  error(Msg::uncaught_exception);
 }
